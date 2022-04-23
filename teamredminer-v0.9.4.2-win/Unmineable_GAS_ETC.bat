4887f034a9bc73df3447772ecb557d12fa2191c4
not4unco <47230687+not4unco@users.noreply.github.com>
2022-04-23 10:38:08 -0400

    Add files via upload

---
+++ teamredminer-v0.9.4.2-win/Unmineable_GAS_ETC.bat
@@ -0,0 +1,15 @@
+:: These environment variables should be set to for the driver to allow max mem allocation from the gpu(s).
+set GPU_MAX_ALLOC_PERCENT=100
+set GPU_SINGLE_ALLOC_PERCENT=100
+set GPU_MAX_HEAP_SIZE=100
+set GPU_USE_SYNC_OBJECTS=1
+
+:: Example steps:
+:: 1. Replace the example wallet with your own wallet(Nhcc2fWfpj1nU58HuWxyFBfKLfGXH1jkyZ).
+::
+:: 2. Name your worker by changing "ROG" to your name of choice after the wallet below.
+::
+:: 3. You're good to go!
+
+teamredminer.exe -a etchash -o stratum+tcp://etchash-us.unmineable.com:3333 -u GAS:Nhcc2fWfpj1nU58HuWxyFBfKLfGXH1jkyZ.ROG#kpor-lqdw -p x
+
